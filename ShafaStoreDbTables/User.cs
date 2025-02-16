414f582e3dca4a2876092b35890c10a5d6dfd0c3
aanixx <anna.moskslenko.kr.rog@gmail.com>
2025-02-16 18:59:45 +0200

    Create table User

--- ShafaStoreDbTables/User.cs
+++ ShafaStoreDbTables/User.cs
@@ -1,4 +1,11 @@
-﻿namespace ShafaStoreDbTables
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ShafaStoreDbTables
 {
     public class User
     {
@@ -6,12 +13,15 @@
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Patronymic { get; set; }
-        public string Login {  get; set; }
+        public string Login { get; set; }
         public string Password { get; set; }
         public string Gender { get; set; }
         public string City { get; set; }
         public string Email { get; set; }
-        public int PhoneNumber { get; set; }
-        public int Subscriber { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<string> Subscriber { get; set; }
+        public ICollection<string> Sales { get; set; }
+        public ICollection<string> Reviews { get; set; }
+        public SqlBinary Photo { get; set; }
     }
 }
