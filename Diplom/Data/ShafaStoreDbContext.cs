69cb9b885392ec8f90600a8410f41453124cee06
aanixx <anna.moskslenko.kr.rog@gmail.com>
2025-02-16 20:00:51 +0200

    same

--- Diplom/Data/ShafaStoreDbContext.cs
+++ Diplom/Data/ShafaStoreDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using ShafaStoreDbTables;
 namespace Diplom.Data
 {
     public class ShafaStoreDBContext : DbContext
     {
+        internal DbSet<User> users {  get; set; }
+        public ShafaStoreDBContext(DbContextOptions<ShafaStoreDBContext> options) : base(options) { }
     }
 }
--- Diplom/Program.cs
+++ Diplom/Program.cs
@@ -1,14 +1,31 @@
+using Diplom.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.IO;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+string? path = builder.Configuration.GetConnectionString("ShafaStore");
+
+
+if (path != null)
+{
+    builder.Services.AddDbContext<ShafaStoreDBContext>(str => str.UseSqlServer(path));
+}
+
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -18,8 +35,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseSession();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
