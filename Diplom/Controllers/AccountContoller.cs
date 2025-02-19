c42bbb8fdf15188d153d2ead8227ab2da8e90d8e
aanixx <anna.moskslenko.kr.rog@gmail.com>
2025-02-19 19:58:29 +0200

    correct

--- Diplom/Controllers/AccountContoller.cs
+++ Diplom/Controllers/AccountContoller.cs
@@ -1,15 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Diplom.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShafaStoreDbTables;
+using Diplom.Data.IdentityContext;
 
 namespace Diplom.Controllers
 {
     public class AccountContoller : Controller
     {
+        SingleUser? user;
+        UserManager<SingleUser>? _userManager;
 
-
+        public AccountContoller(UserManager<SingleUser>? userManager)
+        {
+            _userManager = userManager;
+        }
+     
         [HttpGet]
         public IActionResult Registration()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registration(RegViewModel? rgModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (rgModel != null)
+                {
+                        user = await _userManager.FindByEmailAsync(rgModel.Email);
+
+                        if (user == null)
+                        {
+                            user = new SingleUser();
+                            user.Login = rgModel.Login;
+                            user.Email = rgModel.Email;
+
+                            if (rgModel.Password != null && _userManager != null)
+                            {
+                                IdentityResult res = await _userManager.CreateAsync(user, rgModel.Password);
+                            }
+                        }
+                        else
+                        {
+                            return RedirectToAction("UserExists", "Account");
+                        }
+                    }
+            }
+
+            return RedirectToAction("Registration", "Account");
         }
     }
 }
---
+++ Diplom/Data/IdentityContext/SingleUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Diplom.Data.IdentityContext
+{
+    public class SingleUser : IdentityUser
+    {
+        public string? Login { get; set; }
+
+        public override string?  PhoneNumber { get; set; }
+        public override string? Email { get; set; }
+    }
+}
